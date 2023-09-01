cef2a5874a972345ae7a5f89c516db277f0e2926
AsDazai <128502641+AsDazai@users.noreply.github.com>
2023-09-01 08:40:35 +0530

    Add files via upload

---
+++ Explora 2.0/timepass.py
@@ -0,0 +1,14 @@
+#-variable
+print(r'telusko \n rocks')
+
+n = 5
+s = 5
+
+num = n + s
+print(num)
+
+#____________________________________________________________#
+#id
+
+x = 'shantanu'
+print(id(x))
